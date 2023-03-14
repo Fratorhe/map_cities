134f8184edf5971363d92ae9524f7503ccfe788a
Fran <fratorhe@vki.ac.be>
2023-03-13 21:23:21 -0500

    commented heroku methods

--- telegram_bot.py
+++ telegram_bot.py
@@ -103,20 +103,20 @@ def handle_section(call, dict_data_call):
     bot.send_message(call.from_user.id, s, parse_mode="MarkdownV2")
 
 
-@server.route("/" + TOKEN, methods=["POST"])
-def getMessage():
-    json_string = request.get_data().decode("utf-8")
-    update = telebot.types.Update.de_json(json_string)
-    bot.process_new_updates([update])
-    return "!", 200
-
-
-@server.route("/")
-def webhook():
-    bot.remove_webhook()
-    bot.set_webhook(url="https://torresh.pythonanywhere.com/" + TOKEN)
-    return "!", 200
+# @server.route("/" + TOKEN, methods=["POST"])
+# def getMessage():
+#     json_string = request.get_data().decode("utf-8")
+#     update = telebot.types.Update.de_json(json_string)
+#     bot.process_new_updates([update])
+#     return "!", 200
+#
+#
+# @server.route("/")
+# def webhook():
+#     bot.remove_webhook()
+#     bot.set_webhook(url="https://torresh.pythonanywhere.com/" + TOKEN)
+#     return "!", 200
 
 
 if __name__ == "__main__":
-    server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    server.run(debug=True)
